9ae25df66e6228459e86bb0e4d2912d833435058
Nguyễn Khắc Hào <nkhachao@gmail.com>
2022-03-19 01:33:39 +0700

    Add logging for demo app

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -1,11 +1,21 @@
 from References.DialoGPT import DialoGPT as Chatbot
 import pyttsx3
+from datetime import datetime
 from flask import Flask, render_template, request
 import threading
 
 chatbot = Chatbot()
 app = Flask(__name__)
 engine = pyttsx3.init(driverName='nsss')
+
+
+def log(text):
+    with open('Demo/Log.txt', 'a') as file:
+        now = datetime.now()
+        current_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+        file.write('\n(' + current_time + ') ' + text)
+
 
 def speak(text):
     if engine._inLoop:
@@ -21,27 +31,28 @@ def index():
 
 @app.route("/get_response")
 def get_bot_response():
-    utterance = request.args.get('msg')
+    message = request.args.get('msg')
 
-    while True:
-        try:
-            response = chatbot.reply(utterance)
-            t1 = threading.Thread(target=speak, args=(response,))
-            t1.start()
-            t1.join()
-            return response
-        except:
-            return 'I crashed and restarted!'
+    try:
+        response = chatbot.reply(message)
+        t1 = threading.Thread(target=speak, args=(response,))
+        t1.start()
+        t1.join()
+    except:
+        response = 'I crashed and restarted!'
+
+    log('Human: ' + message)
+    log('Bot: ' + response)
+    return response
 
 
 @app.route("/reset_conversation", methods=['POST'])
 def reset_conversation():
     global chatbot
     chatbot.reset()
-    print('New conversation started')
+    log('------ New conversation started ------')
+    print('------ New conversation started ------')
     return ''
-
-
 
 
 if __name__ == "__main__":

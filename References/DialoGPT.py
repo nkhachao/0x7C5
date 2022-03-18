3aa178c2cacc5767832be6168b2e09987d883658
Nguyễn Khắc Hào <nkhachao@gmail.com>
2022-03-18 20:13:20 +0700

    Switch to conversation trimming instead of full reset

--- References/DialoGPT.py
+++ References/DialoGPT.py
@@ -6,13 +6,11 @@ class DialoGPT:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
         self.model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
-        self.chat_history_ids = torch.LongTensor([[]])
-        self.previous_response = ''
+        self.chat_history = []
 
     def reply(self, message):
-        # Add the new user input tokens to the chat history
-        new_message_ids = self.tokenizer.encode(message + self.tokenizer.eos_token, return_tensors='pt')
-        self.chat_history_ids = torch.cat([self.chat_history_ids, new_message_ids], dim=-1)
+        previous_response = self.chat_history[-1] if self.chat_history else ''
+        self.chat_history.append(message)
 
         # Transformer-based language generation models tend to repeat the same thing. This is a known issue.
         # https://github.com/microsoft/DialoGPT/issues/45
@@ -20,21 +18,20 @@ class DialoGPT:
         # Generate a new response until we get one that doesn't repeat the last response
         # Generate a new response until we get one that doesn't repeat the user's message
 
-        response, chat_history_ids, score = self.generate_candidate_response()
+        response, score = self.generate_candidate_response()
 
         # If the context is becoming too much for the bot to handle, reset conversation history
-        if score > -0.03:
-            self.chat_history_ids = torch.cat([torch.LongTensor([[]]), new_message_ids], dim=-1)
-            response, chat_history_ids, score = self.generate_candidate_response()
-            print('Model overwhelmed by large context. Cleared conversation history')
+        while score > -0.1:
+            self.forget_oldest_response()
+            response, score = self.generate_candidate_response()
+            print('Model overwhelmed by large context. Cleared 1 conversation turn. '
+                  'Current conversation length: ', len(self.chat_history))
 
-        while response.lower() == self.previous_response.lower() or response.lower() == message.lower():
-            response, chat_history_ids, score = self.generate_candidate_response()
+        while response.lower() == previous_response.lower() or response.lower() == message.lower():
+            response, score = self.generate_candidate_response()
 
         print(score)
-
-        self.previous_response = response
-        self.chat_history_ids = chat_history_ids
+        self.chat_history.append(response)
 
         return response
 
@@ -45,16 +42,26 @@ class DialoGPT:
         # But human likes to be surprised by the responses, so some randomness is needed
         # Using beam search with 2 beams and sampling
 
-        outputs = self.model.generate(self.chat_history_ids, max_length=1000,
+        outputs = self.model.generate(self.message_to_ids(self.chat_history), max_length=1000,
                                       pad_token_id=self.tokenizer.eos_token_id,
                                       num_beams=2, do_sample=True,
                                       return_dict_in_generate=True, output_scores=True)
 
         chat_history_ids = outputs['sequences']
         score = outputs['sequences_scores'][0].item()
-        chat_history = self.tokenizer.decode(chat_history_ids[0])
 
-        return chat_history.split('<|endoftext|>')[-2], chat_history_ids, score
+        return self.ids_to_messages(chat_history_ids)[-1], score
+
+    def ids_to_messages(self, chat_history_ids):
+        decoded_string = self.tokenizer.decode(chat_history_ids[0])
+        return decoded_string.split(self.tokenizer.eos_token)[:-1]
+
+    def message_to_ids(self, chat_history):
+        input_string = self.tokenizer.eos_token.join(chat_history) + self.tokenizer.eos_token
+        return self.tokenizer.encode(input_string, return_tensors='pt')
+
+    def forget_oldest_response(self):
+        self.chat_history = self.chat_history[2:]
 
     def reset(self):  # Reinitialize this class is really slow. So we need a reset method instead
-        self.chat_history_ids = torch.LongTensor([[]])
+        self.chat_history = []
